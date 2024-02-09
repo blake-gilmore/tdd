f0dc12156dd5b0cf80630bd6844e4e0e2af81a0a
blake-gilmore <80655017+blake-gilmore@users.noreply.github.com>
2024-02-08 19:27:13 -0800

    Initial Commit

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,39 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app 
+
+# we need to import the file that contains the status codes
+from src import status 
+
+class CounterTest(TestCase):
+    """Counter tests"""
+    def setUp(self):
+        self.client = app.test_client() 
+          
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        client = app.test_client()
+        result = client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+     
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+        
+    def test_update_a_counter(self):
+        """Tests updating counters"""
+        result = self.client.post('/counters/update')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        test_counter = result.json['update']
+        result = self.client.put('/counters/update')
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertGreater(result.json['update'], test_counter)
+        
